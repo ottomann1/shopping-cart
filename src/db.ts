4aea32354fda70459500d7f4e60d282edc3dc21c
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-17 14:09:53 +0200

    db connection fixed

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,16 @@
-import { Client } from "pg";
-import { drizzle } from "drizzle-orm";
-
-const client = new Client({
-  host: "localhost",
+import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
+// Create a new PostgreSQL client
+const client = postgres({
+  host: 'localhost',
   port: 5432,
-  user: "postgres",
-  password: "password",
-  database: "postgres",
-  ssl: false, // Disable SSL
+  user: 'postgres',
+  password: 'password',
+  database: 'postgres',
+  ssl: false, // Disable SSL if not using SSL
 });
-client.connect();
+
+// Initialize Drizzle ORM with the connected client
 const db = drizzle(client);
+
 export default db;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import db from "./db";
+import {carts, products, productsInCart} from './schema'
+import express from "express";
 
-const main = async () => {};
-main()
-  .catch(console.error)
-  .finally(() => process.exit());
+const app = express()
+const port = 3000
+
+app.post("/api/carts/", async (req, res)=>{
+  console.log("line 9");
+  
+  await db
+  .insert(carts)
+  .values({
+    totalPrice: 0,
+  })
+  .execute();
+  res.send("hello world")
+})
+
+
+
+app.listen(port,() =>{
+  console.log(`App listening on ${port}`);
+  
+})
