3b7ae551b4ae78a409f461269d02218b27b6a198
Nils Hansson <nilsmenghansson@gmail.com>
2024-07-17 14:48:32 +0200

    created get endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,47 @@
 import db from "./db";
-import {carts, products, productsInCart} from './schema'
+import { carts, products, productsInCart } from "./schema";
 import express from "express";
+import { eq } from "drizzle-orm";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.post("/api/carts/", async (req, res)=>{
+app.post("/api/carts/", async (req, res) => {
   console.log("line 9");
-  
+
   await db
-  .insert(carts)
-  .values({
-    totalPrice: 0,
-  })
-  .execute();
-  res.send("hello world")
-})
+    .insert(carts)
+    .values({
+      totalPrice: 0,
+    })
+    .execute();
+  res.send("hello world");
+});
 
-app.get('/api/carts/:id/', async(req, res) => {
-  console.log('line 21');
-  
-  const cart = await db.select().from(carts).where(carts.cartid.eq(req.params.id)).execute()
+app.get("/api/carts/:id/", async (req, res) => {
+  console.log("line 21");
 
-  res.json(cart)
-})
+  const cart = await db
+    .select()
+    .from(carts)
+    .where(eq(carts.cartid, req.params.id))
+    .execute();
 
+  res.json(cart);
+});
 
-app.listen(port,() =>{
+app.post("/api/carts/:cartId/products/", async (req, res) => {
+  const { cartId } = req.params;
+  const { productId, quantity } = req.body;
+
+  const cart = await db
+    .select()
+    .from(carts)
+    .where(eq(carts.cartid, cartId))
+    .execute();
+
+    const product = 
+});
+app.listen(port, () => {
   console.log(`App listening on ${port}`);
-  
-})
+});
